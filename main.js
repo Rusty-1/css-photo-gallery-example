a5b1c514a9707c4a5ec49b0607131e75dfa389f5
Shaun Rust <shaun@rusty1.net>
2023-07-13 18:46:30 +0200

    Random layout sizes

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 function GeneratePhotos(count){
-    topics = ['Sports', 'Cars', 'Wildlife', 'Sharks', 'Surfing', 'Arts', 'Creative', 'Cities', 'Rugby', 'Diving', 'Festivals', 'Landscapes']
+    let topics = ['Sports', 'Cars', 'Wildlife', 'Sharks', 'Surfing', 'Arts', 'Creative', 'Cities', 'Rugby', 'Diving', 'Festivals', 'Landscapes']
+    let layouts = ['size-small', 'size-medium-1', 'size-small', 'size-medium-2', 'size-small', 'size-large-1', 'size-small', 'size-large-2', 'size-small']
 
     let html = ``;
 
     for(let n = 0; n < count; n++){
         let category = topics[Math.floor(Math.random() * topics.length)]
+        let layout = layouts[Math.floor(Math.random() * layouts.length)]
+
         html += `
-            <div class="gallery-item">
+            <div class="gallery-item ${layout}">
                 <div class="image">
                     <img src="https://source.unsplash.com/1600x900?${category}/${n}" alt="${category}">
                 </div>
